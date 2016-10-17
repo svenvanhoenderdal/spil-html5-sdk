11fb1273873250d00211a5db6617a65f2b2d7b03
Martijn van de Sande <almerelc@gmail.com>
2016-10-17 15:34:14 +0200

    wallet updates

--- example_app/js/main.js
+++ example_app/js/main.js
@@ -75,6 +75,89 @@ $(function(){
         }
     };
 
+    $('#getcurrencies').click(function(){
+
+        var currencies = SpilSDK.getWallet().currencies;
+
+
+
+        var tempFn = doT.template(''+
+            '<div class="panel panel-default currency_panel_{{=it.currencyId}}">'+
+            '<div class="panel-heading" role="tab" id="panel_{{=it.id}}">'+
+            '<div class="panel-title" style="font-size:12px">'+
+            '<a role="button" data-toggle="collapse" href="#body_{{=it.id}}">{{=it.name}}} (id:{{=it.currencyId}})</a>'+
+            '<span class="badge pull-right" >{{=it.currentBalance}}</span>'+
+            '</div>'+
+            '</div>'+
+            '<div id="body_{{=it.id}}" class="panel-collapse collapse">'+
+            '<div class="panel-body">' +
+            '<span>{{=it.currentBalance}}</span>' +
+            '<div class="btn btn-primary decrement">-</div>' +
+            '<input type="amount" class="inputfield" value="1">'+
+            '<div class="btn btn-primary increment">+</div>' +
+            '<div class="btn btn-primary savewallet" data-id="{{=it.currencyId}}" >save</div>' +
+            '<img class="status_image hidden" style="width:40px;" src="img/{{=it.statusImage}}">' +
+            '</div>'+
+            '</div>'+
+            '</div>');
+
+        $('#accordion').on('click','.decrement',function(){
+
+            var valueField = $(this).parent().find('.inputfield');
+
+            $(valueField).val(parseInt(valueField.val())-1);
+        });
+
+        $('#accordion').on('click','.increment',function(){
+
+            var valueField = $(this).parent().find('.inputfield');
+
+            $(valueField).val(parseInt(valueField.val())+1);
+        });
+
+        $('#accordion').on('click','.savewallet',function(){
+            var valueField = $(this).parent().find('.inputfield');
+            //currencyId, delta, reason
+            SpilSDK.addCurrencyToWallet(parseInt($(this).data('id')),valueField.val(),'ItemBought');
+        });
+
+        $('#accordion').html('');
+
+        for(var i=0;i<currencies.length;i++){
+            var currency = currencies[i];
+
+            $('#accordion').append(tempFn({
+                name:currency.name,
+                currentBalance:currency.currentBalance,
+                id:i,
+                currencyId:currency.id,
+                statusImage:'loader.gif'
+            }))
+
+        }
+
+        SpilSDK.setPlayerDataCallbacks({
+            playerDataError:function(error){
+                console.log(error);
+
+            },
+            playerDataUpdated:function(reason, updatedData){
+                var currencies = updatedData.currencies;
+
+                for(var i=0;i<currencies.length;i++){
+                    var currency = currencies[i];
+                    var panel = $('.currency_panel_'+currency.id);
+                    var image = panel.find('.status_image')[0];
+                    image.src = 'img/check.png';
+                    image.classList.remove('hidden');
+
+                    panel.find('.badge').html(currency.currentBalance);
+                }
+            }
+        });
+
+    });
+
     SpilSDK('com.spilgames.slot', '0.0.2', function(){
         console.log('sdk ready');
         SpilSDK.onPackagesUpdated(loadPackages);
@@ -82,13 +165,9 @@ $(function(){
 
         initgame();
 
-        SpilSDK.setPlayerDataCallbacks({
-            playerDataError:function(error){
-                console.log(error);
-            }
-        });
+
 
         //SpilSDK.getWallet();
 
-    }, 'prd');
+    }, 'stg');
 });
